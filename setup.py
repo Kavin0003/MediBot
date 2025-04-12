17520d876e48ce38fd7aa39a0a3d3b7594de4031
Kavin0003 <kavinanand.j0003@gmail.com>
2025-04-12 17:36:27 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Principles of  AI Project',
+    version = '0.1.0',
+    author = 'Kavin Anand',
+    author_email='kavinanand.j0003@gmail.com',
+    packages=find_packages(),
+    install_requires =[]
+
+)
